80b50988d3ab2ca7d5cd7049ffca200c312cf79e
Aynaz Eyvazzadeh <aynazeivazzadeh@gmail.com>
2026-07-18 17:42:10 +0800

    add search box to resulrs page

--- result/result.js
+++ result/result.js
@@ -26,33 +26,7 @@ const addEventOnElements = function(elements, eventType, callback) {
 
 
 
-// scroll
 
-const sections = document.querySelectorAll(".reveal");
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if(entry.isIntersecting){
-
-            entry.target.classList.add("show");
-
-            observer.unobserve(entry.target);
-
-        }
-
-    });
-
-},{
-    threshold:0.15
-});
-
-sections.forEach(section=>{
-
-    observer.observe(section);
-
-});
 
 /**
  * Navbar toggler for mobile
